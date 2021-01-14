91d6f8bc3ef79d13dde55b1a34dc5c30d3ed5c28
Mohamed Farhat <mohamedfarhat01@gmail.com>
2021-01-14 10:59:06 +0400

    first commit

---
+++ src/app/images/images/images.component.ts
@@ -0,0 +1,18 @@
+import { ImageService } from './../../shared/image.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-images',
+  templateUrl: './images.component.html',
+  styles: [
+  ]
+})
+export class ImagesComponent implements OnInit {
+
+  constructor(private service:ImageService) { }
+
+  ngOnInit(): void {
+    this.service.getImageDetailList();
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBazxybWMKouPfoYSb_mZume_7qDNsUro4",
+    authDomain: "myshopdeploy.firebaseapp.com",
+    databaseURL: "https://myshopdeploy.firebaseio.com",
+    projectId: "myshopdeploy",
+    storageBucket: "myshopdeploy.appspot.com",
+    messagingSenderId: "484728817586",
+    appId: "1:484728817586:web:5b68bdcfcc4b898f4786ab",
+    measurementId: "G-HH4D4RWHT1"
+  }
+};
